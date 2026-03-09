f167ac4345b57deff666e95d4b6e6541ea68f518
MADDOS98 <mocanu.andrei@elev.cihcahul.md>
2026-03-09 10:10:03 +0200

    Add Task model and migration for tasks table; update index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,5 +14,6 @@
             <p class="text-center text-secondary mb-3">Aplicație pentru gestionarea activităților.</p>
         </div>
     </div>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body> 
 </html>
