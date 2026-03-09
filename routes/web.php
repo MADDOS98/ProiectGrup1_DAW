396a08a43b7b06677cbc1a44b8439dbfd9934538
Mocanu Andrei <mocanu.andrei@elev.cihcahul.md>
2026-03-09 22:20:39 +0200

    Change POST to PUT for task update route and ensure proper DELETE method for task destruction

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
 Route::get('/{id}', [TaskController::class, 'edit'])->name('tasks.edit');
 Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
-Route::post('/{id}', [TaskController::class, 'update'])->name('tasks.update');
-Route::post('/{id}/delete', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::put('/{id}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/{id}/delete', [TaskController::class, 'destroy'])->name('tasks.destroy');
