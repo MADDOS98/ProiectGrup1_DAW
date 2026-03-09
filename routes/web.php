44a0047bd8189de90abc9928b32decb20e13bbff
LiviuD20 <doina.liviu@elev.cihcahul.md>
2026-03-09 22:16:06 +0200

    Update task edit form to use PUT method for updating tasks and adjust delete form placement

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index'); 
 Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
 Route::get('/{id}', [TaskController::class, 'edit'])->name('tasks.edit');
 Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
-Route::post('/{id}', [TaskController::class, 'update'])->name('tasks.update');
+Route::put('/{id}', [TaskController::class, 'update'])->name('tasks.update');
 Route::delete('/{id}/delete', [TaskController::class, 'destroy'])->name('tasks.destroy');
